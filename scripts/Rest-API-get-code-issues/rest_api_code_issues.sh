fa6ad3b113f1db0748ba4df2301b0bdebf1af224
MiklosCsecsi_Snyk <105910607+mcsnyk@users.noreply.github.com>
2022-09-26 10:05:28 +0200

    Update rest_api_code_issues.sh

--- scripts/Rest-API-get-code-issues/rest_api_code_issues.sh
+++ scripts/Rest-API-get-code-issues/rest_api_code_issues.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# LOW SEVERITY ISSUES
 # get code results with the API
-curl --request GET "https://api.snyk.io/rest/orgs/6391f850-81f8-48fc-9cd4-aa8c186c6ff0/issues?project_id=71f558ae-894e-4ff9-8656-55840e0fc78a&severity=high&type=code&version=2022-04-06%7Eexperimental" \
+curl --request GET "https://api.snyk.io/rest/orgs/3ee0d99d-1651-4b78-b9a4-a2658bd1815e/issues?project_id=7107beaa-3011-4821-bb2a-bb0e4226b731&severity=low&type=code&version=2022-04-06%7Eexperimental" \
 --header "Accept: application/vnd.api+json" \
---header "Authorization: Token bf9b9173-fdf6-4d2a-b603-ebc6e6b0f341" | tee code_results.json
+--header "Authorization: Token <Your Snyk Token>" | tee code_results_low.json
 
 # We can also leverage the python script: easier to configure
 #pip3 install -r requirements.txt
@@ -12,4 +13,23 @@ curl --request GET "https://api.snyk.io/rest/orgs/6391f850-81f8-48fc-9cd4-aa8c18
 # create a post request - send the results to AWS Lambda
 curl --location --request POST 'https://vxy2zw7203.execute-api.us-west-2.amazonaws.com/default/SplunkOfficial' \
 --header 'Content-Type: application/json' \
---data @code_results.json
+--data @code_results_low.json
+
+# MEDIUM SEVERITY ISSUES
+curl --request GET "https://api.snyk.io/rest/orgs/3ee0d99d-1651-4b78-b9a4-a2658bd1815e/issues?project_id=7107beaa-3011-4821-bb2a-bb0e4226b731&severity=medium&type=code&version=2022-04-06%7Eexperimental" \
+--header "Accept: application/vnd.api+json" \
+--header "Authorization: Token <Your Snyk Token>" | tee code_results_med.json
+
+curl --location --request POST 'https://vxy2zw7203.execute-api.us-west-2.amazonaws.com/default/SplunkOfficial' \
+--header 'Content-Type: application/json' \
+--data @code_results_med.json
+
+
+# HIGH SEVERITY ISSUES
+curl --request GET "https://api.snyk.io/rest/orgs/3ee0d99d-1651-4b78-b9a4-a2658bd1815e/issues?project_id=7107beaa-3011-4821-bb2a-bb0e4226b731&severity=high&type=code&version=2022-04-06%7Eexperimental" \
+--header "Accept: application/vnd.api+json" \
+--header "Authorization: Token <Your Snyk Token>" | tee code_results_high.json
+
+curl --location --request POST 'https://vxy2zw7203.execute-api.us-west-2.amazonaws.com/default/SplunkOfficial' \
+--header 'Content-Type: application/json' \
+--data @code_results_high.json
